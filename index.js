2fe08a9c16c0eda3e6135d83c370bb1d8ae0e8cd
AJ <ajspeller@gmail.com>
2018-04-14 13:23:38 -0400

    Abstraction

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
+// use abstration to ...
+// 1) hide the details
+// 2) show the essentials`
+
 // constructor function
 function Circle(radius) {
     this.radius = radius;
+    this.defaultLocation = { // hide
+        x: 0,
+        y: 0
+    };
+    this.computeOptimumLocation = function () { // hide
+
+    };
     this.draw = function () {
+        this.computeOptimumLocation()
         console.log('draw');
     }
 }
 
 const circle = new Circle(10);
-
-
-for (let key in circle) {
-    if (typeof circle[key] !== 'function')
-    console.log(key, circle[key]);
-}
-
-const keys = Object.keys(circle);
-console.log(keys);
-
-if ('radius' in  circle) {
-    console.log('Circle has a radius');
-}
