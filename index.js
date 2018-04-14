b451841730ffa59b60c71c6908163d29e478157d
AJ <ajspeller@gmail.com>
2018-04-14 13:01:14 -0400

    Value vs Reference Type

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-// constructor function
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function () {
-        console.log('draw');
-    }
+// Value Types -- copy by value
+// ===========
+// Number
+// String
+// Boolean
+// Symbol
+// undefined
+// null
+
+// Reference Types -- copy by reference
+// ===============
+// Object
+// Function
+// Array
+
+let x = 10;
+let y = x;
+
+x = 20;
+
+// x & y are independent
+console.log(x);
+console.log(y);
+
+console.log(`-------`);
+
+let a = { value: 10 };
+let b = a;
+
+a.value = 20;
+
+console.log(a.value);
+console.log(b.value);
+
+let number = 10; 
+
+function increase(number){
+    number++;
 }
 
-Circle.call({}, 1);  // the this keyword references {}, arguments passed in one at a time
-Circle.apply({}, [1]); // the this keyword references {}, arguments in an array
-
-const another = new Circle(1);
+increase(number);
+console.log(`number: `, number);
 
 
+let obj = { value: 10 }; 
 
-const newCircle = new Circle(1);
-newCircle.draw();
+function increase(obj){
+    obj.value++;
+}
 
-const Circle1 = new Function('radius',
-    `
-    this.radius = radius;
-    this.draw = function () {
-        console.log('draw');
-    }
-`);
-
-const circle1 = new Circle1(1);
+increase(obj);
+console.log(`obj: `, obj);
