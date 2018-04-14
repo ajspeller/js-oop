de161f3bde46a67302b31c6e0de88dddee9350c3
AJ <ajspeller@gmail.com>
2018-04-14 12:51:28 -0400

    Functions are Objects

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// factory function
-let createCircle = (radius) => {
-    return {
-        radius,
-        draw: function () {
-            console.log('draw');
-        }
-    }
-}
-
-const circle = createCircle(1);
-
-
 // constructor function
 function Circle(radius) {
     this.radius = radius;
@@ -19,11 +6,22 @@ function Circle(radius) {
     }
 }
 
-const newCircle = new Circle(1);
+Circle.call({}, 1);  // the this keyword references {}, arguments passed in one at a time
+Circle.apply({}, [1]); // the this keyword references {}, arguments in an array
 
+const another = new Circle(1);
+
+
+
+const newCircle = new Circle(1);
 newCircle.draw();
 
-let x = {}  // let x = new Object();
-new String() // '' "" ``
-new Number() // 1, 2, 3 ...
-new Boolean() // true false
+const Circle1 = new Function('radius',
+    `
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+`);
+
+const circle1 = new Circle1(1);
