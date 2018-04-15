4af6be1e52056d5d5e6f3b1211d9ae5da4fa4df9
AJ <ajspeller@gmail.com>
2018-04-14 22:47:10 -0400

    Prototypical Inheritance

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function Shape() {
+
+}
+
+Shape.prototype.duplicate = function () {
+    console.log('duplicate');
+}
+
 function Circle(radius) {
     // instance members
     this.radius = radius;
@@ -7,21 +15,12 @@ function Circle(radius) {
     }
 }
 
+Circle.prototype = Object.create(Shape.prototype);
+
 // prototype member
 Circle.prototype.draw = function () {
     console.log('draw');
 }
 
-const c1 = new Circle(1);
-
-c1.draw();
-
-console.log(Object.keys(c1)); // only returns instance members
-
-// returns all members 
-for (key in c1) {
-    console.log(key); // returns both the instance and prototype members
-}
-
-console.log(c1.hasOwnProperty('radius')); // true .. instance member
-console.log(c1.hasOwnProperty('draw')); // false .. prototype member
+const s = new Shape();
+const c = new Circle(3);
