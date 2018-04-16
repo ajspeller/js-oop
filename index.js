83174ea2941b8de6fa36c781a25c183d78a17ea0
AJ <ajspeller@gmail.com>
2018-04-15 22:43:08 -0400

    Intermediate Function Inheritance

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ Shape.prototype.duplicate = function () {
     console.log('duplicate');
 }
 
+function extend(Child, Parent) {
+    // resetting the prototype
+    Child.prototype = Object.create(Parent.prototype);
+    // whenever you reset the prototype, you should also reset the constructor
+    Child.prototype.constructor = Child; 
+}
+
 function Circle(radius, color) {
     Shape.call(this, color); // super constructor
     // instance members
@@ -16,16 +23,19 @@ function Circle(radius, color) {
     }
 }
 
-// resetting the prototype
-Circle.prototype = Object.create(Shape.prototype);
+extend(Circle, Shape);
 
-// whenever you reset the prototype, you should also reset the constructor
-Circle.prototype.constructor = Circle;
 
 // prototype member
 Circle.prototype.draw = function () {
     console.log('draw');
 }
+
+function Square(size) {
+    this.size = size;
+}
+
+extend(Square, Shape);
 
 const s = new Shape();
 const c = new Circle(3);
