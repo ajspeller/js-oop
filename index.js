e2a19fab1b00282bab884e7f05c34b2420556b3f
AJ <ajspeller@gmail.com>
2018-04-15 23:05:43 -0400

    Method Overriding

--- index.js
+++ index.js
@@ -1,41 +1,23 @@
-function Shape(color) {
-    this.color = color;
+function extend(Child, Parent) {
+    // resetting the prototype
+    Child.prototype = Object.create(Parent.prototype);
+    // whenever you reset the prototype, you should also reset the constructor
+    Child.prototype.constructor = Child;
 }
+
+function Shape() {}
 
 Shape.prototype.duplicate = function () {
     console.log('duplicate');
 }
 
-function extend(Child, Parent) {
-    // resetting the prototype
-    Child.prototype = Object.create(Parent.prototype);
-    // whenever you reset the prototype, you should also reset the constructor
-    Child.prototype.constructor = Child; 
+function Circle(radius, color) {}
+
+extend(Circle, Shape); 
+
+Circle.prototype.duplicate = function () {
+    Shape.prototyoe.duplicate.call(this);
+    console.log('duplicate circle');
 }
 
-function Circle(radius, color) {
-    Shape.call(this, color); // super constructor
-    // instance members
-    this.radius = radius;
-
-    this.move = function () {
-        console.log('draw');
-    }
-}
-
-extend(Circle, Shape);
-
-
-// prototype member
-Circle.prototype.draw = function () {
-    console.log('draw');
-}
-
-function Square(size) {
-    this.size = size;
-}
-
-extend(Square, Shape);
-
-const s = new Shape();
 const c = new Circle(3);
