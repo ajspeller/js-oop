04c48a1f5b396367b07777b79a876c5f201247d1
AJ <ajspeller@gmail.com>
2018-04-16 00:40:43 -0400

    Polymorphic Inheritance Exercise

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-function mixin(target, ...sources) {
-    Object.assign(target, ...sources);
-}
-
-const canEat = {
-    eat: function () {
-        this.hunger--;
-        console.log(`eating`);
-    }
-};
-const canWalk = {
-    walk: function () {
-        this.hunger--;
-        console.log(`walking`);
-    }
-};
-
-const canSwim = {
-    swim: function () {
-        console.log(`swimming`);
+function HtmlElement() {
+    this.click = function () {
+        console.log(`click`);
     }
 }
 
-function Person() {}
-mixin(Person.prototype, canEat, canSwim, canWalk);
-const person = new Person();
-console.log(person);
+HtmlElement.prototype.focus = function () {
+    console.log(`focus`);
+}
 
-function GoldFish() {}
-mixin(GoldFish.prototype, canEat, canSwim);
-const goldFish = new GoldFish();
-console.log(goldFish);
+const htmlElement = new HtmlElement();
+
+console.log(htmlElement);
+
+function HtmlSelectElement(items) {
+    this.items = items ? items : [];
+    this.addItem = function (item) {
+        this.items.push(item);
+    };
+    this.removeItem = function (item) {
+        let idx = items.indexOf(item);
+        if (idx !== -1) {
+            this.items.splice(idx, 1);
+        }
+    }
+    this.displayItems = function() {
+        console.table(items);
+    }
+}
+
+HtmlSelectElement.prototype = new HtmlElement();
+HtmlSelectElement.constructor = HtmlSelectElement;
+
+const h = new HtmlSelectElement([1, 2, 3]);
+
+
+console.log(h);
+
+h.displayItems();
+h.addItem(7);
+h.displayItems();
+h.removeItem(7);
+h.displayItems();
