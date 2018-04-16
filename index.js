02a08560c52309e574e0b30c0cb5cfe010ea41e6
AJ <ajspeller@gmail.com>
2018-04-16 19:57:39 -0400

    Getters and Setters

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const _radius = new WeakMap(); // the keys can be garbage collected
-const _move = new WeakMap();
 
 class Circle {
     constructor(radius) {
         _radius.set(this, radius); // private
-        _move.set(this, () => {
-            console.log(`move`, this);
-        });
+
+        // Object.defineProperty(this, 'radius', {
+        //     get: function () {
+
+        //     }
+        // });
     }
-    draw() {
-        console.log(_radius.get(this));
-        _move.get(this)();
-        console.log(`draw`);
+    get radius() {
+        return _radius.get(this);
+    }
+    set radius(value){
+        if (value <= 0) {
+            throw new Error(`invalis raidus`);
+        }
+        _radius.set(this.value);
     }
 }
 
-const c = new Circle(9);
 
-console.log(c);
-c.draw();
+const c = new Circle(9);
