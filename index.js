7ad037b53b2dd10bf6349c53c5631e9780e48c7e
AJ <ajspeller@gmail.com>
2018-04-16 20:34:50 -0400

    Stack Implementation

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-class Shape {
-    move() {
-        console.log(`shape move`);
+const _items = new WeakMap();
+
+class Stack {
+
+    constructor() {
+        _items.set(this, []);
+    }
+
+    peek() {
+        const items = _items.get(this);
+        if (items.length === 0) {
+            throw new Error(`Stack is empty`);
+        }
+        return items[items.length - 1];
+    }
+
+    push(value) {
+        _items.get(this).push(value);
+    }
+
+    pop() {
+        const items = _items.get(this);
+        if (_items.get(this).length === 0) {
+            throw new Error(`Stack is empty`);
+        }
+        return _items.get(this).pop();
+    }
+
+    get count() {
+        const items = _items.get(this);
+        return items.length;
     }
 }
 
-class Circle extends Shape {
-    move() {
-        super.move();
-        console.log(`circle move`);
-    }
-}
-
-const c = new Circle();
-
-console.log(c);
-
-c.move();
+var stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push('a');
+console.log(stack.count);
+console.log(stack.peek());
+stack.pop();
+console.log(stack.peek());
