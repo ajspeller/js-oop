fa4f11785afc04e5162c20fe98ef13199817bf0c
AJ <ajspeller@gmail.com>
2018-04-16 22:47:27 -0400

    CommonJS Module

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-const _radius = new WeakMap();
 
-class Circle {
-    constructor(radius) {
-        _radius.set(this, radius);
-    }
-
-    draw() {
-        console.log(`Circle with radius ` + _radius.get(this));
-    }
-}
+const Circle = require('./circle');
 
 const c = new Circle(10);
-// hack to get around the fact that _radius is private, 
-// module will protect this private variable
-_radius.get(c); 
+
 c.draw();
